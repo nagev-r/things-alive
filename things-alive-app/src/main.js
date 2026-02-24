bb044cfdbf441e64a01b03b035c1f71b6a4d5ba2
Nageline Vallon-Rodriguez <78504669+nagev-r@users.noreply.github.com>
2026-02-24 21:05:14 +0000

    Fix default landing and panel animation to reveal main page

--- things-alive-app/src/main.js
+++ things-alive-app/src/main.js
@@ -1,25 +1,34 @@
-import './style.css'
 
 const landing = document.getElementById("landing");
 const app = document.getElementById("app");
 const enterBtn = document.getElementById("enterBtn");
 const loader = document.getElementById("loader");
-
-/**Pre-load class is removed from body */
-document.documentElement.classList.remove('preload');
+const panels = document.querySelector('.door-panels');
 
 window.addEventListener("load", () => {
     landing.classList.remove("hidden");
 })
 
+  function openDoors(){
+    panels.classList.add('open');
+  }
+  
 enterBtn.addEventListener("click", () => {
 
     enterBtn.disabled = true;
     loader.classList.add("show");
-
+    
+  
     setTimeout(() => {
-        landing.classList.add("hidden");
-        app.classList.remove("hidden");    
-    }, 2000);
+      landing.classList.remove("is-active");
+      app.classList.add("is-active");
+      
+      setTimeout(() => {
+        openDoors()
+    
+      
+      }, 500)
+  }, 600);
     
   })
+
